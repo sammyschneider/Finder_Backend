e45f2de0f80f9a9bdc669a9b5cf4997271fe21a5
Sammy Schneider <SammySchneider@Sammys-MacBook-Air.local>
2020-10-31 09:09:09 -0700

    added allowed host

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'fnder.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'fnder.herokuapp.com']
 
 
 # Application definition
